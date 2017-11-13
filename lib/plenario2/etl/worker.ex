8b9a596a60773ceb4d9ceb6615eff1ca91a5c720
Jesus Bracho <jbracho@uchicago.edu>
2017-11-13 10:38:29 -0500

    Add some basic docs for the worker

--- lib/plenario2/etl/worker.ex
+++ lib/plenario2/etl/worker.ex
@@ -1,3 +1,38 @@
 defmodule Plenario2.Etl.Worker do
+  @moduledoc """
+  A `GenServer` responsible for ingesting a single dataset. It dies when it
+  has succesfully ingested a dataset or when it errors. It conveys infromation
+  about the ongoing ingest through updates to its state, which can be inspected
+  with `:sys.get_state/1`.
+  """
+
   use GenServer
+
+  @doc """
+  Entrypoint for the `Worker` `GenServer`. Saves you the hassle of writing out
+  `GenServer.start_link`. Calls this module's `init/1` function.
+
+  ## Example
+
+    iex> alias Plenario2.Etl.Worker
+    nil
+    iex> worker = 
+    ...>   Worker.start_link(%{
+    ...>     name: "reports",
+    ...>     source_url: "https://reports.org/download"
+    ...>     data_set_fields: %{}
+    ...>   })
+  """
+  def start_link(state) do
+    GenServer.start_link(__MODULE__, state)
+  end
+
+  @doc """
+  Runs once when the `Worker` is starting and sets the state of the server.
+  The `state` is a map that contains all the information necessary to ingest a
+  dataset.
+  """
+  def init(state) do
+    {:ok, state}
+  end
 end
