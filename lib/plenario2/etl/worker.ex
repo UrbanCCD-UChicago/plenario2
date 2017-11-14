fa09a260f2c9c7cf45bdc13e05905e085874d49f
Jesus Bracho <jbracho@uchicago.edu>
2017-11-14 15:46:34 -0500

    Check in parallel ingest

--- lib/plenario2/etl/worker.ex
+++ lib/plenario2/etl/worker.ex
@@ -6,6 +6,7 @@ defmodule Plenario2.Etl.Worker do
   with `:sys.get_state/1`.
   """
 
+  import Ecto.Adapters.SQL, only: [query!: 3]
   import Ecto.Migration, only: [create: 2, table: 1, timestamps: 0]
   use GenServer
 
@@ -35,6 +36,7 @@ defmodule Plenario2.Etl.Worker do
   The `state` is a map that contains all the information necessary to ingest a
   dataset.
   """
+  @spec init(state :: map) :: {:ok, map}
   def init(state) do
     {:ok, state}
   end
@@ -44,6 +46,7 @@ defmodule Plenario2.Etl.Worker do
   `state` to include a `worker_downloaded_file_path` key with the location
   of the downloaded file.
   """
+  @spec download(state :: map) :: map
   def download(state) do
     %HTTPoison.Response{body: body} = HTTPoison.get!(state[:source_url])
     path = "/tmp/#{state[:name]}.csv"
@@ -56,9 +59,34 @@ defmodule Plenario2.Etl.Worker do
   a schema defined by the `dataset_set_fields` key in our `state`. If it does
   exist but columns have been added, modify the table accordingly.
   """
+  @spec stage(state :: map) :: map
   def stage(state) do
-    sql = EEx.eval_file(@create_table_template, [state: state])
-    Ecto.Adapters.SQL.query!(Plenario2.Repo, sql, [])
+    sql = EEx.eval_file(@create_table_template, state: state)
+    query!(Plenario2.Repo, sql, [])
     state
+  end
+
+  @doc """
+  Upsert dataset rows from the file specified in `state`. Operations are
+  performed in parallel on chunks of the file stream.
+  """
+  @spec load(state :: map) :: map
+  def load(state) do
+    File.stream!(state[:worker_downloaded_file_path])
+    |> Stream.chunk_every(100)
+    |> Enum.map(fn chunk -> spawn(__MODULE__, :upsert, [chunk]) end)
+    |> Enum.map(fn pid ->
+         receive do
+           {^pid, result} -> result
+         end
+       end)
+  end
+
+  @doc """
+  Upsert a chunk of rows.
+  """
+  @spec upsert(rows :: list) :: :ok
+  def upsert(rows) do
+    :ok
   end
 end
--- test/plenario2/etl/worker_test.exs
+++ test/plenario2/etl/worker_test.exs
@@ -1,25 +1,36 @@
 defmodule Plenario2.Etl.WorkerTest do
   alias Plenario2.Etl.Worker
-  use ExUnit.Case
+  import Ecto.Adapters.SQL, only: [query!: 3]
+  use Plenario2.DataCase
 
   @fixture_name "calendars"
   @fixture_source_url "http://insight.dev.schoolwires.com/HelpAssets/C2Assets/C2Files/C2ImportCalEventSample.csv"
   @fixture_tmp_path "/tmp/calendars.csv"
 
   test "Worker downloads file to correct location" do
-    state = Worker.download(%{
-      name: @fixture_name,
-      source_url: @fixture_source_url
-    })
+    state =
+      Worker.download(%{
+        name: @fixture_name,
+        source_url: @fixture_source_url
+      })
 
     assert state[:worker_downloaded_file_path] === @fixture_tmp_path
-    assert File.exists? @fixture_tmp_path
+    assert File.exists?(@fixture_tmp_path)
   end
 
   test "Worker stages table correctly" do
-    Worker.stage(%{name: "hello", fields: [
-      foo: "string",
-      bar: "integer"
-    ]})
+    Worker.stage(%{
+      name: "hello",
+      fields: [
+        foo: "text",
+        bar: "integer"
+      ]
+    })
+
+    %Postgrex.Result{columns: columns, rows: rows} =
+      query!(Plenario2.Repo, ~s{select * from "hello"}, [])
+
+    assert columns === ["bar", "foo"]
+    assert rows === []
   end
 end
