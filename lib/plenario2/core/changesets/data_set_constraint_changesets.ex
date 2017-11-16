c49cc6da40d1f4e72dfee32bbe56f50a3ba6450c
Vince Forgione <vforgione@uchicago.edu>
2017-11-16 11:30:32 -0600

    validating the fields exist for a constraint

--- lib/plenario2/core/changesets/data_set_constraint_changesets.ex
+++ lib/plenario2/core/changesets/data_set_constraint_changesets.ex
@@ -1,12 +1,13 @@
 defmodule Plenario2.Core.Changesets.DataSetConstraintChangesets do
   import Ecto.Changeset
-  alias Plenario2.Core.Actions.MetaActions
+  alias Plenario2.Core.Actions.{MetaActions, DataSetFieldActions}
   alias Plenario2.Core.Schemas.Meta
 
   def create(struct, params) do
     struct
     |> cast(params, [:field_names, :meta_id])
     |> validate_required([:field_names, :meta_id])
+    |> _validate_field_names()
     |> cast_assoc(:meta)
     |> _set_name()
   end
@@ -25,5 +26,24 @@ defmodule Plenario2.Core.Changesets.DataSetConstraintChangesets do
     constraint_name = Enum.join(pieces, "_")
 
     changeset |> put_change(:constraint_name, constraint_name)
+  end
+
+  ##
+  # validation
+
+  defp _validate_field_names(changeset) do
+    meta_id = get_field(changeset, :meta_id)
+    field_names = get_field(changeset, :field_names)
+
+    meta = MetaActions.get_from_pk(meta_id)
+    fields = DataSetFieldActions.list_for_meta(meta)
+    known_field_names = for f <- fields, do: f.name
+
+    is_subset = field_names |> Enum.all?(fn (name) -> Enum.member?(known_field_names, name) end)
+    if is_subset do
+      changeset
+    else
+      changeset |> add_error(:field_names, "Field names must exist as registered fields of the dataset")
+    end
   end
 end
--- test/plenario2/core/data_set_constraint_actions_test.exs
+++ test/plenario2/core/data_set_constraint_actions_test.exs
@@ -1,6 +1,6 @@
 defmodule DataSetConstraintActionsTest do
   use ExUnit.Case, async: true
-  alias Plenario2.Core.Actions.{DataSetConstraintActions, MetaActions, UserActions}
+  alias Plenario2.Core.Actions.{DataSetFieldActions, DataSetConstraintActions, MetaActions, UserActions}
   alias Plenario2.Repo
 
   setup do
@@ -8,13 +8,25 @@ defmodule DataSetConstraintActionsTest do
 
     {:ok, user} = UserActions.create("Test User", "password", "test@example.com")
     {:ok, meta} = MetaActions.create("Chicago Tree Trimming", user.id, "https://www.example.com/chicago-tree-trimming")
+    DataSetFieldActions.create(meta.id, "date", "date")
+    DataSetFieldActions.create(meta.id, "location", "text")
+    DataSetFieldActions.create(meta.id, "event_id", "text")
 
     [meta: meta]
   end
 
-  test "create a new constraint", context do
+  test "create a new constraint with one field", context do
+    {:ok, cons} = DataSetConstraintActions.create(context.meta.id, ["event_id"])
+    assert cons.constraint_name == "unique_constraint_chicago_tree_trimming_event_id"
+  end
+
+  test "create a new constraint with two fields", context do
     {:ok, cons} = DataSetConstraintActions.create(context.meta.id, ["date", "location"])
     assert cons.constraint_name == "unique_constraint_chicago_tree_trimming_date_location"
+  end
+
+  test "creating a constraint with a field that doesn't exist in the dataset fails", context do
+    {:error, _} = DataSetConstraintActions.create(context.meta.id, ["some_other_id"])
   end
 
   test "list constraints for a meta", context do
