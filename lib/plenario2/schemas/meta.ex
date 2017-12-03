bf434e56d58e3b9fd74bbccabc8e10546c115779
Vince Forgione <vforgione@uchicago.edu>
2017-12-02 19:18:38 -0600

    Added FSM to Meta Schema
    - added fsm workflow for meta states
    - tested these suckers
    
    closes #43

--- lib/plenario2/schemas/meta.ex
+++ lib/plenario2/schemas/meta.ex
@@ -1,10 +1,35 @@
 defmodule Plenario2.Schemas.Meta do
   use Ecto.Schema
+  use EctoStateMachine,
+    states: [:new, :needs_approval, :ready, :erred],
+    events: [
+      [
+        name: :submit_for_approval,
+        from: [:new],
+        to: :needs_approval
+      ], [
+        name: :approve,
+        from: [:needs_approval],
+        to: :ready
+      ], [
+        name: :disapprove,
+        from: [:needs_approval],
+        to: :new
+      ], [
+        name: :mark_erred,
+        from: [:ready],
+        to: :erred
+      ], [
+        name: :mark_fixed,
+        from: [:erred],
+        to: :ready
+      ]
+    ]
 
   schema "metas" do
     field(:name, :string)
     field(:slug, :string)
-    field(:state, :string)
+    field(:state, :string, default: "new")
     field(:description, :string)
     field(:attribution, :string)
     field(:source_url, :string)
