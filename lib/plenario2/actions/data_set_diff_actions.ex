7ef26570e76d1224b1ee0dd90f2add4c2fa0ed94
Vince Forgione <vforgione@uchicago.edu>
2018-01-05 10:04:44 -0600

    DataSetDiffActions are now Struct/ID Agnostic
    updates #92

--- lib/plenario2/actions/data_set_diff_actions.ex
+++ lib/plenario2/actions/data_set_diff_actions.ex
@@ -6,15 +6,45 @@ defmodule Plenario2.Actions.DataSetDiffActions do
 
   import Ecto.Query
 
+  import Plenario2.Guards, only: [is_id: 1]
+
   alias Plenario2.Changesets.DataSetDiffChangesets
-  alias Plenario2.Schemas.{DataSetDiff, Meta}
+  alias Plenario2.Schemas.{DataSetDiff, Meta, DataSetConstraint, EtlJob}
   alias Plenario2.Repo
+
+  @typedoc """
+  Parameter is an ID attribute
+  """
+  @type id :: String.t | integer
+
+  @typedoc """
+  Returns a tuple of :ok, DataSetDiff or :error, Ecto.Changeset
+  """
+  @type ok_diff :: {:ok, DataSetDiff} | {:error, Ecto.Changeset.T}
 
   @doc """
   Creates a new instance of DataSetDiff.
   """
-  @spec create(meta_id :: integer, constraint_id :: integer, etl_job_id :: integer, column :: String.t, original :: any, updated :: any, changed_on :: %DateTime{}, constraint_values :: %{}) :: {:ok, %DataSetDiff{} | :error, Ecto.Changeset.t}
-  def create(meta_id, constraint_id, etl_job_id, column, original, updated, changed_on, constraint_values) do
+  @spec create(meta :: Meta | id, constraint :: DataSetConstraint | id, etl_job :: EtlJob | id, column :: String.t, original :: any, updated :: any, changed_on :: DateTime, constraint_values :: %{}) :: ok_diff
+  def create(meta, constraint, etl_job, column, original, updated, changed_on, constraint_values) do
+    meta_id =
+      case is_id(meta) do
+        true -> meta
+        false -> meta.id
+      end
+
+    constraint_id =
+      case is_id(constraint) do
+        true -> constraint
+        false -> constraint.id
+      end
+
+    etl_job_id =
+      case is_id(etl_job) do
+        true -> etl_job
+        false -> etl_job.id
+      end
+
     params = %{
       meta_id: meta_id,
       data_set_constraint_id: constraint_id,
@@ -33,6 +63,17 @@ defmodule Plenario2.Actions.DataSetDiffActions do
   @doc """
   Lists all the diffs related to a given Meta.
   """
-  @spec list_for_meta(meta :: %Meta{}) :: [%DataSetDiff{}]
-  def list_for_meta(meta), do: Repo.all(from d in DataSetDiff, where: d.meta_id == ^meta.id)
+  @spec list_for_meta(meta :: Meta | id) :: list(DataSetDiff)
+  def list_for_meta(meta) do
+    meta_id =
+      case is_id(meta) do
+        true -> meta
+        false -> meta.id
+      end
+
+    Repo.all(
+      from d in DataSetDiff,
+      where: d.meta_id == ^meta_id
+    )
+  end
 end
