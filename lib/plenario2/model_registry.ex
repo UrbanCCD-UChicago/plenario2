c9f16d44b96ef11ef816e1ce6bf7938ff78eb29c
Jesus Bracho <jbracho@uchicago.edu>
2018-01-24 12:21:57 -0500

    Add documentation and doctests

--- lib/plenario2/model_registry.ex
+++ lib/plenario2/model_registry.ex
@@ -10,20 +10,71 @@ defmodule Plenario2.ModelRegistry do
     "timestamptz" => :naive_datetime
   }
 
-  # Client api 
+  @doc """
+  Entrypoint for the registry. `args` is used to set the initial state of a
+  registry server and is meant to be a map. An empty map is used by default
+  but a prepopulated map can be provided to preload the server.
 
-  def start_link(args) do
-    GenServer.start_link(__MODULE__, args, name: __MODULE__)
+  Note that by default, this function names the spawned server `__MODULE__1
+  under the assumption that only one registry is in use. This is for usability
+  purposes so that clients do not need to explicitly specify the registry name.
+
+  ## Examples
+
+      iex> alias Plenario2.ModelRegistry
+      iex> {status, _pid} = ModelRegistry.start_link(%{}, :test)
+      iex> status == :ok
+      true
+
+      iex> alias Plenario2.ModelRegistry
+      iex> {status, _pid} = ModelRegistry.start_link(%{
+      ...>   1 => :"Model.ExistingModel",
+      ...>   "slug" => :"Model.ExistingModel"
+      ...> }, :test)
+      iex> status == :ok
+      true
+
+  """
+  def start_link(args, name \\ __MODULE__) do
+    GenServer.start_link(__MODULE__, args, name: name)
   end
 
-  def lookup(slug) do
-    GenServer.call(__MODULE__, {:lookup, slug})
-  end
-
-  # Server callbacks
-
+  @doc """
+  Called by `start_link`, this function is used to set the intial state of the
+  server.
+  """
   def init(args) do
     {:ok, args}
+  end
+
+  @doc """
+  Asks the registry to return an atom that identifies an ecto schema that
+  corresponds to an instance of `Meta`. A module is created on the fly for 
+  the first time a schema is requested.
+
+  The returned value can be used to compose queries with Ecto's query api.
+
+  ## Examples
+
+      iex> alias Plenario2.ModelRegistry
+      iex> alias Plenario2.Actions.MetaActions
+      iex> alias Plenario2Auth.UserActions
+      iex> {:ok, user} = UserActions.create("test", "password", "test@email.com")
+      iex> {:ok, meta} = MetaActions.create("SomeData", user.id(), "source")
+      iex> ModelRegistry.lookup(meta.id())
+      :"Model.somedata"
+
+      iex> alias Plenario2.ModelRegistry
+      iex> alias Plenario2.Actions.MetaActions
+      iex> alias Plenario2Auth.UserActions
+      iex> {:ok, user} = UserActions.create("test", "password", "test@email.com")
+      iex> {:ok, meta} = MetaActions.create("otherdata", user.id(), "source")
+      iex> ModelRegistry.lookup(meta.slug())
+      :"Model.otherdata"
+
+  """
+  def lookup(slug, pid \\ __MODULE__) do
+    GenServer.call(pid, {:lookup, slug})
   end
 
   def handle_call({:lookup, slug}, _sender, state) do
@@ -42,8 +93,6 @@ defmodule Plenario2.ModelRegistry do
   def handle_call(request, sender, state) do
     super(request, sender, state)
   end
-
-  # Server logic 
 
   defp register(meta) do
     module = "Model." <> Slug.slugify(meta.name())
---
+++ test/plenario2/model_registry_test.ex
@@ -0,0 +1,4 @@
+defmodule Plenario2.ModelRegistryTest do
+  use Plenario2.DataCase
+  doctest Plenario2.ModelRegistry
+end
