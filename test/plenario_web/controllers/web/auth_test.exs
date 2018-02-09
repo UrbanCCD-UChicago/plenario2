e07fd87f151ecae68ff3ccec0c17e3f29c280d0e
Vince Forgione <vforgione@uchicago.edu>
2018-02-09 12:13:13 -0600

    added auth controller tests

---
+++ test/plenario_web/controllers/web/auth_test.exs
@@ -0,0 +1,92 @@
+defmodule PlenarioWeb.Web.Testing.AuthControllerTest do
+  use PlenarioWeb.Testing.ConnCase, async: true
+
+  describe "index" do
+    @tag :auth
+    test "when already authenticated", %{conn: conn} do
+      conn
+      |> get(auth_path(conn, :index))
+      |> html_response(:found)
+    end
+
+    @tag :anon
+    test "when anonymous", %{conn: conn} do
+      response =
+        conn
+        |> get(auth_path(conn, :index))
+        |> html_response(:ok)
+
+      assert response =~ "Login"
+      assert response =~ "Register"
+    end
+  end
+
+  describe "login" do
+    @tag :auth
+    test "with good credentials", %{conn: conn, reg_user: user} do
+      params = %{
+        "user" => %{
+          "email" => user.email,
+          "password" => "password"  # omg so secure!
+        }
+      }
+
+      conn
+      |> post(auth_path(conn, :login, params))
+      |> html_response(:found)
+    end
+
+    @tag :anon
+    test "with bad credentials", %{conn: conn} do
+      params = %{
+        "user" => %{
+          "email" => "justsomeperson@nowhere.com",
+          "password" => "whatever"
+        }
+      }
+
+      conn
+      |> post(auth_path(conn, :login, params))
+      |> html_response(:bad_request)
+    end
+  end
+
+  describe "register" do
+    @tag :anon
+    test "as a new user", %{conn: conn} do
+      params = %{
+        "user" => %{
+          "email" => "justsomeperson@nowhere.com",
+          "password" => "whatever",
+          "name" => "some person"
+        }
+      }
+
+      conn
+      |> post(auth_path(conn, :register, params))
+      |> html_response(:found)
+    end
+
+    @tag :auth
+    test "with a taken email address", %{conn: conn, reg_user: user} do
+      params = %{
+        "user" => %{
+          "email" => user.email,
+          "password" => "whatever",
+          "name" => "some person"
+        }
+      }
+
+      conn
+      |> post(auth_path(conn, :register, params))
+      |> html_response(:bad_request)
+    end
+  end
+
+  @tag :auth
+  test "logout", %{conn: conn} do
+    conn
+    |> post(auth_path(conn, :logout))
+    |> html_response(:found)
+  end
+end
