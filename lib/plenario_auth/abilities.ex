92bdd8d421e83d4070f041dd4ced5f43736707b7
Vince Forgione <vforgione@uchicago.edu>
2018-02-09 09:29:21 -0600

    cleaned up abilities

--- lib/plenario_auth/abilities.ex
+++ lib/plenario_auth/abilities.ex
@@ -1,7 +1,7 @@
 defmodule PlenarioAuth.Abilities do
   @moduledoc """
-  Defines auhtorization capabilities for users in relation to resources. For example, we only
-  want the user who owns a meta to be able to edit it.
+  Defines authorization abilities for users in relation to resources. For
+  example, we only want the user who owns a meta to be able to edit it.
   """
 
   alias Plenario.Schemas.{
@@ -18,50 +18,93 @@ defmodule PlenarioAuth.Abilities do
   defimpl Canada.Can, for: User do
 
     @doc """
-    For all create, edit, delete actions of a Meta, ensure the user is the owner
-    otherwaise disallow.
+    Admin users can perform any action, regardless of who owns the resource.
     """
-    def can?(nil, _, _ = %Meta{}), do: false
-    def can?(%User{id: user_id}, _, meta = %Meta{}), do: meta.user_id == user_id
+    def can?(%User{is_admin: true}, _, _), do: true
 
     @doc """
-    For all create, edit, delete actions of a DataSetField, ensure the user
-    is the owner of the related Meta otherwaise disallow.
+    Anonymous users can only access the Meta index and show.
     """
-    def can?(nil, _, _ = %DataSetField{}), do: false
-    def can?(%User{id: user_id}, _, field = %DataSetField{}) do
-      meta = MetaActions.get(field.meta_id)
+    def can?(nil, action, %Meta{}) when action in [:index, :show], do: true
+    def can?(nil, _, %Meta{}), do: false
+
+    @doc """
+    Authenticated users can only access the Meta index and show, unless they
+    are the owner of the Meta.
+    """
+    def can?(%User{id: user_id}, _, %Meta{user_id: user_id}), do: true
+    def can?(%User{id: user_id}, action, %Meta{}) when action in [:index, :show], do: true
+    def can?(%User{id: user_id}, _, %Meta{}), do: false
+
+    @doc """
+    Anonymous users have no access to DataSetField.
+    """
+    def can?(nil, _, %DataSetField{}), do: false
+
+    @doc """
+    Authenticated users who own the DataSetField's parent Meta are the only
+    users able to access it.
+    """
+    def can?(%User{id: user_id}, _, %DataSetField{meta_id: meta_id}) do
+      meta = MetaActions.get(meta_id)
       meta.user_id == user_id
     end
 
     @doc """
-    For all create, edit, delete actions of a VirtualDateField, ensure the user
-    is the owner of the related Meta otherwaise disallow.
+    Anonymous users have no access to VirtualDateField.
     """
-    def can?(nil, _, _ = %VirtualDateField{}), do: false
-    def can?(%User{id: user_id}, _, field = %VirtualDateField{}) do
-      meta = MetaActions.get(field.meta_id)
+    def can?(nil, _, %VirtualDateField{}), do: false
+
+    @doc """
+    Authenticated users who own the VirtualDateField's parent Meta are the only
+    users able to access it.
+    """
+    def can?(%User{id: user_id}, _, %VirtualDateField{meta_id: meta_id}) do
+      meta = MetaActions.get(meta_id)
       meta.user_id == user_id
     end
 
     @doc """
-    For all create, edit, delete actions of a VirtualPointField, ensure the user
-    is the owner of the related Meta otherwaise disallow.
+    Anonymous users have no access to VirtualPointField.
     """
-    def can?(nil, _, _ = %VirtualPointField{}), do: false
-    def can?(%User{id: user_id}, _, field = %VirtualPointField{}) do
-      meta = MetaActions.get(field.meta_id)
+    def can?(nil, _, %VirtualPointField{}), do: false
+
+    @doc """
+    Authenticated users who own the VirtualPointField's parent Meta are the only
+    users able to access it.
+    """
+    def can?(%User{id: user_id}, _, %VirtualPointField{meta_id: meta_id}) do
+      meta = MetaActions.get(meta_id)
       meta.user_id == user_id
     end
 
     @doc """
-    For all create, edit, delete actions of a UniqueConstraint, ensure the user
-    is the owner of the related Meta otherwaise disallow.
+    Anonymous users have no access to UniqueConstraint.
     """
-    def can?(nil, _, _ = %UniqueConstraint{}), do: false
-    def can?(%User{id: user_id}, _, constraint = %UniqueConstraint{}) do
-      meta = MetaActions.get(constraint.meta_id)
+    def can?(nil, _, %UniqueConstraint{}), do: false
+
+    @doc """
+    Authenticated users who own the UniqueConstraint's parent Meta are the only
+    users able to access it.
+    """
+    def can?(%User{id: user_id}, _, %UniqueConstraint{meta_id: meta_id}) do
+      meta = MetaActions.get(meta_id)
       meta.user_id == user_id
     end
+
+    @doc """
+    Anonymous users can only access the User index and show.
+    """
+    def can?(nil, action, %User{}) when action in [:index, :show], do: true
+    def can?(nil, _, %User{}), do: false
+
+    @doc """
+    Authenticated users can only access another User's index and show, unless
+    they are the requested User -- then obviously they can do whatever with
+    themselves... I mean it's a free country and all.
+    """
+    def can?(%User{id: user_id}, _, %User{id: user_id}), do: true
+    def can?(%User{}, action, %User{}) when action in [:index, :show], do: true
+    def can?(%User{}, _, %User{}), do: false
   end
 end
