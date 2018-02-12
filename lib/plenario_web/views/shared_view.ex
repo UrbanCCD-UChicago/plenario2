019a48e735084cf2f7cb71fd15cc0418c2c112ef
Vince Forgione <vforgione@uchicago.edu>
2018-02-12 15:39:49 -0600

    added shared view for doughnut charts

--- lib/plenario_web/views/shared_view.ex
+++ lib/plenario_web/views/shared_view.ex
@@ -15,4 +15,43 @@ defmodule PlenarioWeb.SharedView do
     assigns = Keyword.merge(defaults, opts)
     render(PlenarioWeb.SharedView, "map.html", assigns)
   end
+
+  @red "255, 99, 132"
+  @blue "54, 162, 235"
+  @yellow "255, 206, 86"
+  @green "75, 192, 192"
+  @purple "153, 102, 255"
+
+  def render_doughnut(key_values, opts \\ []) do
+    defaults = [
+      chart_id: "donut",
+      height: 200
+    ]
+    opts = Keyword.merge(defaults, opts)
+
+    labels = for {key, _} <- key_values, do: key
+    data = for {_, value} <- key_values, do: value
+    len_data = length(data)
+    backgrounds =
+      Stream.cycle([@red, @blue, @yellow, @green, @purple])
+      |> Enum.take(len_data)
+      |> Enum.map(fn c -> bgrnd_color(c) end)
+    borders =
+      Stream.cycle([@red, @blue, @yellow, @green, @purple])
+      |> Enum.take(len_data)
+      |> Enum.map(fn c -> border_color(c) end)
+
+    assigns = Keyword.merge(opts, [
+      labels: labels,
+      data: data,
+      backgrounds: backgrounds,
+      borders: borders
+    ])
+    IO.inspect(assigns)
+    render(PlenarioWeb.SharedView, "doughnut.html", assigns)
+  end
+
+  defp bgrnd_color(base), do: "rgba(#{base}, 0.2)"
+
+  defp border_color(base), do: "rgba(#{base}, 1)"
 end
