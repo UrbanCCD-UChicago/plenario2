4f80e320aaa2f146895a512430528ea4f271b7e9
Vince Forgione <vforgione@uchicago.edu>
2018-02-16 11:34:03 -0600

    Fixed Location Parsing Bug
    I didn't include tests for parsing _location_ -- just lat/lon pairs.
    Derp.
    
    Fixes #174

--- test/plenario/data_set_actions_test.exs
+++ test/plenario/data_set_actions_test.exs
@@ -19,10 +19,12 @@ defmodule Plenario.Testing.DataSetActionsTest do
     {:ok, day} = DataSetFieldActions.create(meta, "day", "integer")
     {:ok, lat} = DataSetFieldActions.create(meta, "lat", "float")
     {:ok, lon} = DataSetFieldActions.create(meta, "lon", "float")
+    {:ok, loc} = DataSetFieldActions.create(meta, "loc", "text")
 
     {:ok, vdf} = VirtualDateFieldActions.create(meta.id, yr.id, month_field_id: mo.id, day_field_id: day.id)
 
     {:ok, vpf} = VirtualPointFieldActions.create(meta.id, lat.id, lon.id)
+    {:ok, vpf2} = VirtualPointFieldActions.create(meta.id, loc.id)
 
     {:ok, uc} = UniqueConstraintActions.create(meta.id, [pk.id])
 
@@ -37,6 +39,7 @@ defmodule Plenario.Testing.DataSetActionsTest do
         lon: lon,
         vdf: vdf,
         vpf: vpf,
+        vpf2: vpf2,
         uc: uc
       ]
     }
@@ -47,9 +50,9 @@ defmodule Plenario.Testing.DataSetActionsTest do
 
     insert = """
     INSERT INTO "#{meta.table_name}" VALUES
-      (1, 12.3, 2018, 1, 1, 10.1, 20.9),
-      (2, 12.2, 2018, 1, 1, 10.1, 20.8),
-      (3, 12.1, 2018, 1, 1, 10.1, 20.7);
+      (1, 12.3, 2018, 1, 1, 10.1, 20.9, '(10.1, 20.9)'),
+      (2, 12.2, 2018, 1, 1, 10.1, 20.8, '(10.1, 20.8)'),
+      (3, 12.1, 2018, 1, 1, 10.1, 20.7, '(10.1, 20.7)');
     """
     {:ok, _} = Ecto.Adapters.SQL.query(Repo, insert)
 
@@ -61,15 +64,16 @@ defmodule Plenario.Testing.DataSetActionsTest do
     assert length(result.rows) == 3
     first = List.first(result.rows)
     assert first == [
-      1, 12.3, 2018, 1, 1, 10.1, 20.9,
+      1, 12.3, 2018, 1, 1, 10.1, 20.9, "(10.1, 20.9)",
       {{2018, 1, 1}, {0, 0, 0, 0}}, # virtual date
-      %Geo.Point{coordinates: {20.9, 10.1}, srid: 4326}  # virtual point
+      %Geo.Point{coordinates: {20.9, 10.1}, srid: 4326},  # virtual point
+      %Geo.Point{coordinates: {20.9, 10.1}, srid: 4326}
     ]
 
     # check unique key
     insert = """
     INSERT INTO "#{meta.table_name}" VALUES
-      (1, 12.4, 2018, 1, 1, 10.1, 20.9);
+      (1, 12.4, 2018, 1, 1, 10.1, 20.9, '(10.1, 20.9)');
     """
     {:error, _} = Ecto.Adapters.SQL.query(Repo, insert)
   end
