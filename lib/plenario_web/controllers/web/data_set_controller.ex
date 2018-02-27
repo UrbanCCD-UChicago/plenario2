18aff25446c9439df4a37d8591e5c5ac408985fc
heyzoos <jbracho@uchicago.edu>
2018-02-27 13:08:01 -0500

    Limit the field inspection to the first 10 chunks of information

--- lib/plenario_web/controllers/web/data_set_controller.ex
+++ lib/plenario_web/controllers/web/data_set_controller.ex
@@ -50,12 +50,13 @@ defmodule PlenarioWeb.Web.DataSetController do
   def create(conn, %{"meta" => %{"name" => name, "user_id" => user_id, "source_url" => source_url, "source_type" => source_type}}) do
     case MetaActions.create(name, user_id, source_url, source_type) do
       {:ok, meta} ->
-        try do
+        # try do
+          IO.inspect("trying things")
           field_types = MetaActions.guess_field_types!(meta)
           for {name, type} <- field_types, do: DataSetFieldActions.create(meta, "#{name}", type)
-        rescue
-          _ -> put_flash(conn, :warning, "We couldn't parse the document to generate field definitions.")
-        end
+        # rescue
+        #   _ -> put_flash(conn, :warning, "We couldn't parse the document to generate field definitions.")
+        # end
 
         conn
         |> put_flash(:success, "Created data set #{meta.name}")
