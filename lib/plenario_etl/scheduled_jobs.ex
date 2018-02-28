0ff2c56fd2a9f226f253cf5d4ad0df9a47151b18
Jesus Bracho <jbracho@uchicago.edu>
2018-02-28 10:39:02 -0500

    Restore original meta query

--- lib/plenario_etl/scheduled_jobs.ex
+++ lib/plenario_etl/scheduled_jobs.ex
@@ -8,8 +8,17 @@ defmodule PlenarioEtl.ScheduledJobs do
   require Logger
 
   def refresh_datasets() do
-    now = DateTime.utc_now()
-    metas = Repo.all(from m in Meta, where: m.next_import < ^now)
+    offset = Application.get_env(:plenario, :refresh_offest)
+    starts = DateTime.utc_now()
+    ends = Timex.shift(starts, offset)
+
+    metas = Repo.all(
+      from m in Meta,
+        where: m.refresh_starts_on <= ^starts,
+        where: is_nil(m.refresh_ends_on) or m.refresh_ends_on >= ^ends,
+        where: m.next_import >= ^starts,
+        where: m.next_import < ^ends
+    )
 
     Logger.info("[#{inspect self()}] [refresh_datasets] Refreshing #{length(metas)} datasets")
 
