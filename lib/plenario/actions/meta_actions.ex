cc8fc6c36e3049e021b620642871200f4e268b2e
Vince Forgione <vforgione@uchicago.edu>
2018-03-06 09:26:32 -0600

    check in

--- lib/plenario/actions/meta_actions.ex
+++ lib/plenario/actions/meta_actions.ex
@@ -4,6 +4,8 @@ defmodule Plenario.Actions.MetaActions do
   Meta schema -- creating, updating, getting, ...
   """
 
+  require Logger
+
   import Ecto.Query
 
   import Plenario.Queries.Utils
@@ -86,7 +88,10 @@ defmodule Plenario.Actions.MetaActions do
   Convenience function for updating a Meta's latest_import attribute.
   """
   @spec update_latest_import(meta :: Meta, timestamp :: DateTime) :: ok_instance
-  def update_latest_import(meta, timestamp), do: MetaActions.update(meta, latest_import: timestamp)
+  def update_latest_import(meta, timestamp) do
+    Logger.info("updating meta '#{meta.name}' latest import to '#{timestamp}'")
+    MetaActions.update(meta, latest_import: timestamp)
+  end
 
   @doc """
   Convenience function for updating a Meta's next_import attribute.
@@ -94,13 +99,25 @@ defmodule Plenario.Actions.MetaActions do
   @spec update_next_import(meta :: Meta) :: ok_instance
   def update_next_import(%Meta{refresh_rate: nil, refresh_interval: nil} = meta), do: {:ok, meta}
   def update_next_import(%Meta{refresh_rate: rate, refresh_interval: interval} = meta) do
-    current =
+    Logger.info("Updating next_import", meta_id: meta.id meta_name: meta.name)
+    next_import =
       case meta.next_import do
         nil -> DateTime.utc_now()
         _ -> meta.next_import
       end
 
-    shifted = Timex.shift(current, [{String.to_atom(rate), interval}])
+    diff = Timex.diff(next_import, DateTime.utc_now(), String.to_atom(rate))
+    next_import =
+      case diff > interval do
+        true ->
+          Logger.info("Next import has fallen out of sync and we're fastforwarding to now", meta_id: meta.id meta_name: meta.name)
+          DateTime.utc_now()
+
+        false -> next_import
+      end
+
+    shifted = Timex.shift(next_import, [{String.to_atom(rate), interval}])
+    Logger.info("Setting next_import to '#{shifted}'", meta_id: meta.id meta_name: meta.name)
     MetaActions.update(meta, next_import: shifted)
   end
 
