dd40921d512d4d8070a8d20b360689c6076cb4c3
Vince Forgione <vforgione@uchicago.edu>
2018-03-07 13:49:05 -0600

    fixed testing fiasco...

--- test/plenario_etl/exporter_test.exs
+++ test/plenario_etl/exporter_test.exs
@@ -19,40 +19,6 @@ defmodule PlenarioEtl.ExporterTest do
   import Ecto.Query
   import Mock
 
-  setup context do
-    # Prevent cross contamination between tests
-    ModelRegistry.clear()
-
-    # Set up user, metadata, and dataset
-    {:ok, meta} = MetaActions.create("test", context.user.id(), "http://example.com", "csv")
-    {:ok, pk_field} = DataSetFieldActions.create(meta.id, "pk", "integer")
-    {:ok, _} = DataSetFieldActions.create(meta.id, "datetime", "timestamptz")
-    {:ok, _} = DataSetFieldActions.create(meta.id, "location", "text")
-    {:ok, _} = DataSetFieldActions.create(meta.id, "data", "text")
-    {:ok, _} = UniqueConstraintActions.create(meta.id, [pk_field.id])
-    DataSetActions.up!(meta)
-
-    # Preload the dataset with usable information
-    insert_sql = """
-    INSERT INTO <%= table_name %>
-      ("data", "datetime", "location", "pk")
-      VALUES
-        ('crackers', '2017-01-01T00:00:00+00:00', '(0, 1)', 1),
-        ('and', '2017-01-02T00:00:00+00:00', '(0, 2)', 2),
-        ('cheese', '2017-01-03T00:00:00+00:00', '(0, 3)', 3)
-    """
-    sql = EEx.eval_string(insert_sql, [table_name: meta.table_name], trim: true)
-    Ecto.Adapters.SQL.query(Plenario.Repo, sql)
-
-    # Set up the export job
-    query = from(m in ModelRegistry.lookup(meta.slug))
-    querystr = inspect(query, structs: false)
-    {:ok, job} = ExportJobActions.create(meta, context.user, querystr, false)
-    job = Repo.preload(job, [:meta, :user])
-
-    %{export_job: job}
-  end
-
   test "export/1 async api", %{export_job: job} do
     with_mock ExAws, request!: fn _a, _b -> :ok end do
       {:ok, task} = Exporter.export_task(job)
