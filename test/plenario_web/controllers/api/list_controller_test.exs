73d755999f6ba95014d43c7a81b06a88e146b0b6
sanil <sanilio@gmail.com>
2018-04-06 11:54:22 -0500

    options tests

--- test/plenario_web/controllers/api/list_controller_test.exs
+++ test/plenario_web/controllers/api/list_controller_test.exs
@@ -18,4 +18,17 @@ defmodule PlenarioWeb.Api.ListControllerTest do
     result = json_response(conn, 200)
     assert result["data"] == []
   end
+
+  test "OPTIONS /api/v2/data-sets status", %{conn: conn} do
+    conn = options(conn, "/api/v2/data-sets")
+    assert conn.status == 204
+  end
+
+  test "OPTIONS /api/v2/data-sets headers", %{conn: conn} do
+    conn = options(conn, "/api/v2/data-sets")
+    headers = Enum.into(conn.resp_headers, %{})
+    assert headers["access-control-allow-methods"] == "GET,HEAD,OPTIONS"
+    assert headers["access-control-allow-origin"] == "*"
+    assert headers["access-control-max-age"] == "300"
+  end
 end
