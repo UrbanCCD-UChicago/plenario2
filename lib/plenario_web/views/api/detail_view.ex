d5b3fda68aa82e8a0c0abe77c635e178294a192b
Jesus Bracho <jbracho@uchicago.edu>
2018-04-06 12:49:16 -0400

    Add slug endpoint (#284)
    * Return all rows for a simple detail query
    
    * Bump patch version number
    
    * Remove detail controller tests for now

--- lib/plenario_web/views/api/detail_view.ex
+++ lib/plenario_web/views/api/detail_view.ex
@@ -1,15 +1,18 @@
+alias PlenarioWeb.Api.Response
+
+
 defmodule PlenarioWeb.Api.DetailView do
   use PlenarioWeb, :api_view
 
-  def render("get.json", _params) do
-    %{}
+  def render("get.json", %{records: records}) do
+    %Response{data: records}
   end
 
-  def render("head.json", _params) do
-    %{}
+  def render("head.json", %{record: record}) do
+    %Response{data: record}
   end
 
-  def render("describe.json", _params) do
-    %{}
+  def render("describe.json", %{records: records}) do
+    %Response{data: records}
   end
 end
