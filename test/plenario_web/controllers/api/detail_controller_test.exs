a6dd66f82c5e4290205c063118aef49d471acabb
heyzoos <jbracho@uchicago.edu>
2018-04-06 12:34:10 -0400

    Remove detail controller tests for now

--- test/plenario_web/controllers/api/detail_controller_test.exs
+++ test/plenario_web/controllers/api/detail_controller_test.exs
@@ -1,18 +1,3 @@
 defmodule PlenarioWeb.Api.DetailControllerTest do
   use PlenarioWeb.Testing.ConnCase
-
-  test "GET /api/v2/detail", %{conn: conn} do
-    conn = get(conn, "/api/v2/detail")
-    assert json_response(conn, 200) == %{}
-  end
-
-  test "GET /api/v2/detail/@head", %{conn: conn} do
-    conn = get(conn, "/api/v2/detail/@head")
-    assert json_response(conn, 200) == %{}
-  end
-
-  test "GET /api/v2/detail/@describe", %{conn: conn} do
-    conn = get(conn, "/api/v2/detail/@describe")
-    assert json_response(conn, 200) == %{}
-  end
 end
