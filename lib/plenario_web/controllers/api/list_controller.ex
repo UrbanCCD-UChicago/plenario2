b6f7af715b05f5fbe01d2af207f06be71001b049
heyzoos <jbracho@uchicago.edu>
2018-04-09 02:03:19 -0400

    Scratch the surface of pagination and query parsing

--- lib/plenario_web/controllers/api/list_controller.ex
+++ lib/plenario_web/controllers/api/list_controller.ex
@@ -6,17 +6,30 @@ defmodule PlenarioWeb.Api.ListController do
 
   import Ecto.Query
 
+  @keywords ["page", "page_number"]
   @associations [:fields, :unique_constraints, :virtual_dates, :virtual_points, :user]
 
   @doc """
   Lists all metadata objects satisfying the provided query.
   """
-  def get(conn, %{}) do
-    metas =
+  def get(conn, params) do
+    {filters, unused} = Map.split(params, @keywords)
+    kw_filters = Enum.map(filters, fn {k, v} -> {String.to_atom(k), v} end)
+
+    page =
       from(meta in Meta, limit: 500)
-      |> Repo.all()
+      |> Repo.paginate(kw_filters)
+
+    entries =
+      page.entries
       |> Enum.map(fn row -> Map.drop(row, @associations) end)
-    render(conn, "get.json", %{metas: metas})
+
+    render(conn, "get.json", %{
+      params: filters,
+      count: length(entries),
+      total_pages: page.total_pages,
+      total_records: page.total_entries,
+      metas: entries})
   end
 
   @doc """
--- lib/plenario_web/views/api/list_view.ex
+++ lib/plenario_web/views/api/list_view.ex
@@ -4,8 +4,23 @@ alias PlenarioWeb.Api.Response
 defmodule PlenarioWeb.Api.ListView do
   use PlenarioWeb, :api_view
 
-  def render("get.json", %{metas: metas}) do
-    %Response{data: metas}
+  def render("get.json", %{
+    count: count,
+    params: params,
+    total_pages: total_pages,
+    total_records: total_records,
+    metas: metas
+  }) do
+    counts = %Response.Meta.Counts{
+      total_pages: total_pages,
+      total_records: total_records,
+      data: count
+    }
+
+    %Response{
+      meta: %Response.Meta{params: params, counts: counts},
+      data: metas
+    }
   end
 
   def render("head.json", %{meta: meta}) do
