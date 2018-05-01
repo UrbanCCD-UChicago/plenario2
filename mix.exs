d1f61537890dec7035b327f1f3a2be0d5d2aa719
Jesus Bracho <jbracho@uchicago.edu>
2018-05-01 12:18:19 -0400

    Bump version number to 0.7.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Plenario.Mixfile do
   def project do
     [
       app: :plenario,
-      version: "0.7.1",
+      version: "0.7.3",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
