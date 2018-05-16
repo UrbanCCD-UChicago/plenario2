19f5b0bbfabe32be26dc4220a2b92ec0d4c41a97
sanil <sanilio@gmail.com>
2018-05-16 13:16:12 -0500

    formatting

--- lib/plenario_web/controllers/api/method_not_allowed_controller.ex
+++ lib/plenario_web/controllers/api/method_not_allowed_controller.ex
@@ -1,18 +1,19 @@
 defmodule PlenarioWeb.Api.MethodNotAllowedController do
-	use PlenarioWeb, :api_controller
+  use PlenarioWeb, :api_controller
 
-	def match(%Plug.Conn{path_info: path_info} = conn, _params) do
-		routes = PlenarioWeb.Router.__routes__()
-			|> Enum.map(&(&1.path))
-			|> Enum.uniq()
+  def match(%Plug.Conn{path_info: path_info} = conn, _params) do
+    routes =
+      PlenarioWeb.Router.__routes__()
+      |> Enum.map(&(&1.path))
+      |> Enum.uniq()
       |> Enum.map(&(&1
       |> String.split("/")
       |> Enum.drop(1)))
 
-		if path_info in routes do
-			send_resp(conn, 405, "Method not allowed")
-		else
-			send_resp(conn, 404, "")
-		end
-	end
+    if path_info in routes do
+      send_resp(conn, 405, "Method not allowed")
+    else
+      send_resp(conn, 404, "")
+    end
+  end
 end
