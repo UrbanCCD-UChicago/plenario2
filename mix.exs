a260f18a57fa896b17050364bc2d8ad54b07ea7a
Jesus Bracho <jbracho@uchicago.edu>
2018-06-22 12:05:11 -0400

    Update minor version number to `0.10.0`

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Plenario.Mixfile do
   def project do
     [
       app: :plenario,
-      version: "0.9.8",
+      version: "0.10.0",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
