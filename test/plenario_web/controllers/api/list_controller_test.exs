cd14d90378fc0160d5eb6352b11638e5787b79fc
Jesus Bracho <jbracho@uchicago.edu>
2018-07-03 08:37:17 -0400

    Address failures cause by always having `data` be a list

--- test/plenario_web/controllers/api/list_controller_test.exs
+++ test/plenario_web/controllers/api/list_controller_test.exs
@@ -101,7 +101,7 @@ defmodule PlenarioWeb.Api.ListControllerTest do
   test "GET /api/v2/data-sets/@head", %{conn: conn} do
     conn = get(conn, "/api/v2/data-sets/@head")
     result = json_response(conn, 200)
-    assert is_map(result["data"])
+    assert is_list(result["data"])
   end
 
   test "GET /api/v2/data-sets/@describe", %{conn: conn} do
