38fa331005097a45e97b676d2d5a67508c14bb57
Benjamin Lodi <brlodi@gmail.com>
2018-07-10 15:24:09 -0400

    FontAwesome tree shaking does not work without extensive add'l config
    I've just pulled it out for now. We can address it again when we rebuild
    the Webpack stack for the umbrella app

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -3,14 +3,9 @@
 /* general node_modules imports */
 import 'popper.js';
 import 'bootstrap';
-
+import '@fortawesome/fontawesome-free/js/all';
 import 'chart.js';
 import 'phoenix_html';
-
-/* Font Awesome Icons */
-import { library } from '@fortawesome/fontawesome-free/js/fontawesome';
-import { lock } from '@fortawesome/fontawesome-free/js/solid';
-import { github } from '@fortawesome/fontawesome-free/js/brands';
 
 /* Local imports */
 // import socket from './socket';
@@ -18,12 +13,6 @@ import './map';
 
 /* CSS imports (needed to force Webpack to bundle them) */
 import '../css/app.scss';
-
-/* Register FontAwesome icons to the library */
-library.add(
-  lock,
-  github,
-);
 
 // For now, just assign these to the global scope to preserve existing code
 window.$ = $;
