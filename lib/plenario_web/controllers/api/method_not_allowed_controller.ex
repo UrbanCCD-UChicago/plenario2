72b6d02fff17982ee2076dd3ee200c9a982ac04e
Vince Forgione <vforgione@uchicago.edu>
2018-07-26 11:23:10 -0500

    Removed God Awful Explode Library
    Just... wow. What an unbelievable pile of turds.
    
    Rather than relying on a library that was clearly developed by a high
    schooler, we're going to be smart about this and **just use the
    functions and modules Plug already gives us**.
    
    Plug gives us the ability to:
    
    - set the resonse content type we want
    - set the status code and message
    - get an appropriate status code by either its integer value or atom
    - get an appropriate message for that code
    - halt further processing
    
    We have no reason to ever consider what the client accepts via our API
    because its documented to be strictly `application/json`. Clients should
    only expect that content type. And overriding the `accept` content type
    the client sends is a massive violation of the client/server contract.
    
    Beyond that, as the title of the issue this solves indicates, **we do
    not serve up JSON+API content**.
    
    This whole thing went way sideways -- mainly because of good intentions
    to be helpful and give clients verbose error messages. This fixes that.
    We _can_ be helpful and not rely on shity anti-patterns at the same
    time.
    
    Fixes #348

--- lib/plenario_web/controllers/api/method_not_allowed_controller.ex
+++ lib/plenario_web/controllers/api/method_not_allowed_controller.ex
@@ -1,6 +1,8 @@
 defmodule PlenarioWeb.Api.MethodNotAllowedController do
   use PlenarioWeb, :api_controller
 
+  import PlenarioWeb.Api.Utils, only: [halt_with: 2]
+
   def match(%Plug.Conn{path_info: path_info} = conn, _params) do
     routes =
       PlenarioWeb.Router.__routes__()
@@ -11,9 +13,9 @@ defmodule PlenarioWeb.Api.MethodNotAllowedController do
       |> Enum.drop(1)))
 
     if path_info in routes do
-      conn |> Explode.with(405, "Method not allowed")
+      conn |> halt_with(:method_not_allowed)
     else
-      conn |> Explode.with(404, "Not found")
+      conn |> halt_with(:not_found)
     end
   end
 end
