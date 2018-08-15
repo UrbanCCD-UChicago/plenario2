39a020480518f37c4025cb8e074f34d305968381
Benjamin Lodi <brlodi@gmail.com>
2018-08-15 12:31:39 -0400

    Overhaul Webpack config; configure & apply linters
    This represents essentially a ground-up rewrite of our Webpack
    configuration to address a number of issues that were reducing or
    eliminating the benefits of building with Webpack and unnecessarily
    bloating the output bundles.
    
    Specific points of interest:
      - Webpack config now exports functions instead of objects to allow for
          the use of environment variables within the config (the standard
          Node exposure of ENV is not available at the time the Webpack
          config is loaded, but when provided as a function the function
          *is* called with the command line environment)
      - Webpack now configured to split bundles as necessary to minimize
          both initial load times and the amount of duplicated code across
          different logical bundles (e.g. common libraries like JQuery are
          now split out into their own bundle automatically so they only get
          downloaded once no matter how many entry points are on the page)
      - Configured Babel appropriately to minimize code repetition across
          output files
      - Updated Browserslist with a more practical set of targets, and
          updated various packages that supposedly read the .browserslistrc
          file to the versions that *actually do it*
      - Included ESLint to our build pipeline. The packages were there, but
          nothing ever ran ESLint unless you manually ran it from the CLI
      - Updated ESLint config with a proper, fairly comprehensive styleguide
      - Added StyleLint to enforce code style of our SCSS files; included in
          build pipeline
      - Reduced somewhat the log output during development builds, since
          many of the sections were uselessly turncated anyway.
      - Quieted the log output during development builds, and made them fail
          on almost all errors to help ensure we don't ship broken builds
      - Added "staging" and "production" flags to the NPM scripts, which are
          available in the Webpack configuration to modify behavior as
          necessary.
        - Implemented S3 URL injection for Webpack assets so our CSS doesn't
            always point to the dev bucket even in production
      - Reimplemented our use of FontAwesome to enable Webpack to tree-shake
          it, reducing its footprint in our output bundle from almost 1MB to
          a few kilobytes
      - Explicitly excluded Moment.js locales from our builds in a way that
          they can still be explicitly imported if needed but otherwise
          won't be included in output bundles, reducing the Moment.js
          footprint in our bundle by ~70%
      - Trimmed down some dependencies that we weren't using or could
          trivially stop using, and added a couple to support compiling
          single-file Vue components
      - Configured Autoprefixer (and banned manual prefixes in the linter)
          to allow us to use only standard syntax in authored SCSS and have
          necessary vendor prefixes handled automatically for our target
          browsers in .browserslistrc
      - Somewhat fixed minification of both JavaScript and CSS
      - Added WebpackBundleAnalyzer to enable visualization of the
          contributing sizes of modules in our bundle. Enabled by default in
          development, but can be easily turned on in production and staging
          builds by a command line flag
      - Assorted changes to templates and SCSS files to support the new
          Webpack settings and to conform to linting rules

--- assets/src/js/explorer.js
+++ assets/src/js/explorer.js
@@ -1,3 +1,17 @@
+/* global $:false */
+
+import * as L from 'leaflet';
+import 'leaflet-draw';
 import Vue from 'vue';
 
-new Vue({}).$mount('#app');
+/* CSS imports (needed to force Webpack to bundle them) */
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-draw/dist/leaflet.draw.css';
+
+// So current Leaflet implementation doesn't break
+window.L = L;
+
+// Only mount Vue once page has loaded
+$(() => {
+  new Vue({}).$mount('#app');
+});
--- assets/webpack/webpack.prod.js
+++ assets/webpack/webpack.prod.js
@@ -1,15 +1,42 @@
-/* eslint-disable import/no-extraneous-dependencies */
-
 const merge = require('webpack-merge');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const commonFunc = require('./webpack.common');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
 
-const common = require('./webpack.common');
+module.exports = (_, argv) => {
+  const common = commonFunc(null, argv);
+  const prod = {
+    mode:    'production',
+    devtool: 'none',
+    stats:   {
+      all:          false,
+      assets:       true,
+      errors:       true,
+      errorDetails: true,
+      warnings:     true,
+      version:      true,
+    },
+    optimization: {
+      splitChunks: { chunks: 'all' },
+    },
+    performance: {
+      // Hard error to avoid outputting a huge file by accident. We specifically only consider asset
+      // types that would have been productively handled by Webpack so we don't error out just
+      // because a key photo or an error message GIF is a little bit too large. NOTE: rather
+      // generous limits are set here, since this is a hard-pass filter, but in general we want to
+      // shoot to keep our bundled assets in the 250kB range.
+      hints:             'error',
+      assetFilter:       fn => /\.(js|css|html?|svg|png)$/.test(fn),
+      maxAssetSize:      524288,
+      maxEntrypointSize: 1048576,
+    },
+    plugins: [
+      // Lint all of our style files (also used in dev with different config)
+      new StyleLintPlugin({
+        files:      'src/**/*.{css,scss,sass,vue}',
+        emitErrors: true,
+      }),
+    ],
+  };
 
-module.exports = merge(common, {
-  devtool: 'source-map',
-  plugins: [
-    new UglifyJSPlugin({
-      sourceMap: true,
-    }),
-  ],
-});
+  return merge(common, prod);
+};
