d7166f2fd2c71547794ba775b972a26faab9b9c7
Jesus Bracho <jbracho@uchicago.edu>
2018-08-23 13:12:00 -0400

    Check in - can't load a dev datasets [skip ci]

--- lib/plenario_web/controllers/api/detail_controller.ex
+++ lib/plenario_web/controllers/api/detail_controller.ex
@@ -29,6 +29,8 @@ defmodule PlenarioWeb.Api.DetailController do
   plug(:check_page)
   plug(:check_order_by, default_order: "asc:row_id")
   plug(:check_filters)
+  # todo need this
+  # plug(:check_group_by)
 
   @spec get(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def get(conn, %{"slug" => slug}) do
@@ -53,6 +55,11 @@ defmodule PlenarioWeb.Api.DetailController do
       )
 
     render_detail(conn, "describe.json", meta)
+  end
+
+  def aggregate(conn, %{"slug" => slug}) do
+    validate_data_set(slug)
+    |> render_data_set_aggregate(conn, "get.json")
   end
 
   defp render_data_set(%Meta{state: "ready"} = meta, conn, view) do
@@ -82,4 +89,35 @@ defmodule PlenarioWeb.Api.DetailController do
   end
 
   defp render_data_set(_, conn, _), do: halt_with(conn, :not_found)
+
+  defp render_data_set_aggregate(%Meta{state: "ready"} = meta, conn, view) do
+    model = ModelRegistry.lookup(meta.slug)
+
+    page = conn.assigns[:page]
+    page_size = conn.assigns[:page_size]
+    group_by_field = conn.params["group_by"] |> String.to_atom()
+
+    # order ∈ {:asc, :desc}
+    {order, order_by} = conn.assigns[:order_by]
+
+
+    # query =
+    #   conn.assigns[:filters]
+    #   |> Enum.reduce(fn {field, operation, value}, query ->
+    #     apply_filter(query, field, operation, value)
+    #   end)
+
+    # try do
+      # data = Repo.paginate(query, page: page, page_size: page_size)
+      # Need to discern significant column here, read in as user arg
+      data = from(model) |> group_by([p], fragment("date_part('month', ?)", field(p, ^group_by_field))) |> select([p], count("*")) |> Repo.all
+      IO.inspect(data)
+      render_detail(conn, view, data)
+    # rescue
+    #   e in [Ecto.QueryError, Ecto.SubQueryError, Postgrex.Error] ->
+    #     halt_with(conn, :bad_request, e.message)
+    # end
+  end
+
+  defp render_data_set_aggregate(_, conn, _), do: halt_with(conn, :not_found)
 end
