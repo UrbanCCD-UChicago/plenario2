5a3f00c06a507220a76e687cc0c586373b9ebbcc
Jesus Bracho <jbracho@uchicago.edu>
2018-08-29 11:55:44 -0400

    Refactor LMap a bit

--- assets/src/js/components/LMap.vue
+++ assets/src/js/components/LMap.vue
@@ -17,6 +17,10 @@ const enabledDrawTools = {
   circle:       false,
 };
 
+const subdomain = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}{r}.png';
+
+const drawOptions = { draw: enabledDrawTools };
+
 export default {
   props: {
     drawTools: { type: Boolean, default: false },
@@ -24,55 +28,64 @@ export default {
 
   data: function () {
     return {
-      drawnItems: null,
       geojson: this.$route.query.geojson,
+      lmap: null,
+      drawableFeatureGroup: null,
+      tileLayer: null,
     }
   },
+
   mounted() {
-    const map = L.map(this.$el).setView([41.8781, -87.6298], 13);
-
-    L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}{r}.png').addTo(map);
-
-    if (this.drawTools) {
-      this.drawnItems = new L.FeatureGroup().addTo(map);
-      new L.Control.Draw({
-        draw: enabledDrawTools,
-        edit: { featureGroup: this.drawnItems },
-      }).addTo(map);
-      map.on('draw:created', this.onDraw);
-
-      this.drawnItems.clearLayers();
-
-      if (this.geojson) {
-        L.geoJSON(JSON.parse(this.geojson)).addTo(this.drawnItems);
-      }
-    }
-  },
-
-  updated: function() {
-    console.log(this.geojson);
-    this.drawnItems.clearLayers();
-    L.geoJSON(this.geojson).addTo(this.drawnItems);
+    this.initMap();
+    this.possiblyInitDrawTools();
+    this.draw(L.geoJSON(JSON.parse(this.geojson)));
   },
 
   methods: {
-    onDraw(event) {
-      this.drawnItems.clearLayers();
-      this.drawnItems.addLayer(event.layer);
 
-      var query = Object.assign({}, this.$route.query, {
-        geojson: JSON.stringify(event.layer.toGeoJSON())
-      });
+    /**
+     * Set our map and tile layer, add the layer to the map.
+     */
+    initMap() {
+      this.lmap = L.map(this.$el).setView([41.8781, -87.6298], 13);
+      this.tileLayer = L.tileLayer(subdomain).addTo(this.lmap);
+    },
 
+    /**
+     * Conditionally display the drawing tools, depends on this.drawTools.
+     */
+    possiblyInitDrawTools() {
+      if (this.drawTools) {
+        this.drawableFeatureGroup = new L.FeatureGroup().addTo(this.lmap);
+        new L.Control.Draw(drawOptions).addTo(this.lmap);
+        this.lmap.on('draw:created', this.onDraw);
+      }
+    },
 
+    /**
+     * Updates the `geojson` query argument in the url.
+     */
+    updateUrl(geojson) {
+      var query = Object.assign({}, this.$route.query, { geojson });
       var clone = JSON.parse(JSON.stringify(query));
+      this.$router.replace({ name: 'search', query: clone });
+    },
 
-      this.$router.replace({
-        name: 'search', 
-        query: clone,
-      });
-    }
+    /**
+     * Draw a layer to the map. It clears away the previous layers.
+     */
+    draw(layer) {
+      this.drawableFeatureGroup.clearLayers();
+      this.drawableFeatureGroup.addLayer(layer);
+    },
+
+    /**
+     * Callback that fires when the user has drawn a polygon on the map.
+     */
+    onDraw(event) {
+      this.draw(event.layer);
+      this.updateUrl(JSON.stringify(event.layer.toGeoJSON()));
+    },
   },
 };
 </script>
-
