c6593259d1835f13a6f986263e3341a40e18b465
Benjamin Lodi <brlodi@gmail.com>
2018-09-07 13:49:30 -0400

    Use Yarn for JavaScript dependency management (#446)
    * 🧶 Switch to Yarn!
    
    Also upgrades some dependencies because switching package installers
    was likely going to break things anyway with the new dep tree. Luckily
    the only necessary change was to explicitly depend on `autoprefixer`.
    
    * Update Phoneix config and package.json to use Yarn
    
    * Update readme to reflect switch to Yarn

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -3,9 +3,6 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
-    // PostCSS includes Autoprefixer, but ESLint tries to resolve it against the
-    // standalone NPM package (which isn't in our dependencies list)
-    // eslint-disable-next-line import/no-extraneous-dependencies
     require('autoprefixer'),
   ],
 };
