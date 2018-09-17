5cead5c049e05d399d9b24386f9541169cce780b
Jesus Bracho <jbracho@uchicago.edu>
2018-09-17 10:13:04 -0400

    Explicitly import the `$` (#451)

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -5,10 +5,13 @@ import 'phoenix_html';
 import 'popper.js';
 import { throttle } from 'lodash-es';
 import '../css/main.scss';
+// This should just be covered by our config file. 
+// https://github.com/webpack/webpack/issues/4258
+import $ from 'jquery';  
 import fontawesomeController from './fontawesome-controller';
 
 // For now, just assign these to the global scope to preserve existing code
-window.$ = $;
+window.jQuery = $; window.$ = $;
 window.Pikaday = Pikaday;
 window.throttle = throttle;
 
